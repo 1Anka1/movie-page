ee8d6929af37822723e016d722fa3288a9bc2d05
Valerii Strilets <valerii.strilets@gmail.com>
2024-02-29 19:01:37 +0200

    small refactor

--- entities/movie/index.ts
+++ entities/movie/index.ts
@@ -3,3 +3,4 @@ export * from './composables/useMoviesByWeekFetch';
 export * from './composables/useMovieLatestFetch';
 export * from './composables/useMoviesTopRatedFetch';
 export * from './models/Movie';
+export { default as MovieCard } from './ui/MovieCard.vue';
